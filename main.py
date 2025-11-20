862240599e788f499101f58796191eb7c3aa6a8e
Allan John Funelas <funelasallanjohn@gmail.com>
2025-11-21 04:55:53 +0800

    feat: Added a status delay timer to avoid flickering labels

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import cv2, mediapipe as mp, math
+import cv2, mediapipe as mp, math, time
 mp_pose = mp.solutions.pose
 mp_draw = mp.solutions.drawing_utils
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 cap = cv2.VideoCapture(0)
 
+stable_status = "GOOD POSTURE"
+statuschange_delay = 2
+statuschange_starttime = None
 # Calibration variables
 ref_nose_shoulder_dist = None
 calibrated = False
@@ -16,10 +19,10 @@ while True:
     res = pose.process(img)
     
     if res.pose_landmarks:
-        # Draw pose landmarks
+        # Draw pose landmarks | Identified Parts of the User From Webcam
         mp_draw.draw_landmarks(frame, res.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-        
         lm = res.pose_landmarks.landmark
+        
         # Get key points
         L = lm[mp_pose.PoseLandmark.LEFT_SHOULDER]
         R = lm[mp_pose.PoseLandmark.RIGHT_SHOULDER]
@@ -39,11 +42,21 @@ while True:
         if calibrated:
             dist_diff = abs(nose_shoulder_dist - ref_nose_shoulder_dist)
             good_posture = dist_diff < 0.02  # threshold for good posture
-            status = "GOOD POSTURE" if good_posture else "BAD POSTURE"
+            current_status = "GOOD POSTURE" if good_posture else "BAD POSTURE"
+
+            if current_status != stable_status:
+                if statuschange_starttime is None:
+                    statuschange_starttime = time.time()
+                elif time.time() - statuschange_starttime > statuschange_delay:
+                    stable_status = current_status
+                    statuschange_starttime = None
+            else:
+                statuschange_starttime = None
+            status = stable_status
         else:
             status = "PRESS 'C' TO CALIBRATE"
         
-        color = (0,255,0) if (calibrated and good_posture) else (0,0,255) if calibrated else (255,255,0)
+        color = (0,255,0) if (calibrated and status == "GOOD POSTURE") else (0,0,255) if (calibrated and status == "BAD POSTURE") else (255,255,0)
         
         # Draw visual feedback
         cv2.line(frame, L_px, R_px, color, 3)  # shoulder line
